6aa8771726c16c2591360304e870dd2de07a66ba
slebaillif@gmail.com <slebaillif@gmail.com>
2024-05-17 08:40:01 +0300

    first commit

---
+++ core/src/com/mygdx/poupoule/MyGdxGame.java
@@ -0,0 +1,93 @@
+package com.mygdx.poupoule;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+
+import java.util.List;
+
+public class MyGdxGame extends ApplicationAdapter {
+    SpriteBatch batch;
+    Texture img;
+    TextureRegion firstRegion;
+    TextureRegion[] green = new TextureRegion[9];
+    List<TextureRegion> theMap;
+    TiledMap currentMap;
+    TiledMap villageMap;
+    TiledMap guildMap;
+    float unitScale = 1 / 16f;
+    float renderRatio;
+    OrthogonalTiledMapRenderer renderer;
+    OrthographicCamera camera;
+    PlayerCoord playerCoord = new PlayerCoord(13, 15);
+    Texture princess;
+    Sprite princessSprite;
+    MyInputProcessor inputProcessor;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+
+        villageMap = new TmxMapLoader().load("TILESET\\village.tmx");
+        guildMap = new TmxMapLoader().load("TILESET\\guild.tmx");
+        currentMap = villageMap;
+
+        princess = new Texture("SPRITES\\HEROS\\PRINCESS\\HEROS_PixelPackTOPDOWN8BIT_Princess Idle D.gif");
+
+        renderer = new OrthogonalTiledMapRenderer(villageMap, unitScale);
+
+        princessSprite = new Sprite(princess);
+
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0, 0, 0, 1);
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
+
+        renderer.getBatch().begin();
+        renderer.getBatch().draw(princessSprite, playerCoord.x, playerCoord.y, 1, 1);
+        renderer.getBatch().end();
+
+        checkCoordinateEvent();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        camera = new OrthographicCamera();
+        renderRatio = (float) (width) / (float) height;
+        camera.setToOrtho(false, 16 * (renderRatio), 16);
+
+        inputProcessor = new MyInputProcessor(camera, villageMap, playerCoord, renderer);
+        Gdx.input.setInputProcessor(inputProcessor);
+    }
+
+    public void checkCoordinateEvent() {
+        if (currentMap == villageMap && playerCoord.x == 15 && playerCoord.y == 13) {
+            // enter guild
+            renderer = new OrthogonalTiledMapRenderer(guildMap, unitScale);
+            playerCoord.x = 12;
+            playerCoord.y = 2;
+            currentMap = guildMap;
+        }
+    }
+}
---
+++ core/src/com/mygdx/poupoule/MyInputProcessor.java
@@ -0,0 +1,98 @@
+package com.mygdx.poupoule;
+
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+import static com.badlogic.gdx.Input.Keys.*;
+
+public class MyInputProcessor implements InputProcessor {
+    private final Camera camera;
+    private final TiledMap theMap;
+    private final PlayerCoord playerCoord;
+    OrthogonalTiledMapRenderer renderer;
+
+
+    public MyInputProcessor(Camera camera, TiledMap theMap, PlayerCoord coord, OrthogonalTiledMapRenderer renderer) {
+        this.camera = camera;
+        this.theMap = theMap;
+        this.playerCoord = coord;
+        this.renderer = renderer;
+    }
+
+    public boolean keyDown(int keycode) {
+        TiledMapTileLayer collisionLayer = (TiledMapTileLayer) theMap.getLayers().get(1);
+        collisionLayer.getName();
+        if (keycode == LEFT) {
+            TiledMapTileLayer.Cell collision = collisionLayer.getCell(playerCoord.x - 1, playerCoord.y);
+            if (collision == null) {
+                playerCoord.x = playerCoord.x - 1;
+                if (!renderer.getViewBounds().contains(0, playerCoord.y)) {
+                    camera.translate(-1, 0, 0);
+                }
+            }
+        } else if (keycode == RIGHT) {
+            TiledMapTileLayer.Cell collision = collisionLayer.getCell(playerCoord.x + 1, playerCoord.y);
+            if (collision == null) {
+                playerCoord.x = playerCoord.x + 1;
+                if (!renderer.getViewBounds().contains(30, playerCoord.y)) {
+                    camera.translate(1, 0, 0);
+                }
+            }
+        } else if (keycode == UP) {
+            TiledMapTileLayer.Cell collision = collisionLayer.getCell(playerCoord.x, playerCoord.y + 1);
+
+            if (collision == null) {
+                playerCoord.y = playerCoord.y + 1;
+                if (!renderer.getViewBounds().contains(playerCoord.x, 20)) {
+                    camera.translate(0, 1, 0);
+                }
+            }
+        } else if (keycode == DOWN) {
+            TiledMapTileLayer.Cell collision = collisionLayer.getCell(playerCoord.x, playerCoord.y - 1);
+
+            if (collision == null) {
+                playerCoord.y = playerCoord.y - 1;
+                if (!renderer.getViewBounds().contains(playerCoord.x, 0)) {
+                    camera.translate(0, -1, 0);
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    public boolean touchDown(int x, int y, int pointer, int button) {
+        return false;
+    }
+
+    public boolean touchUp(int x, int y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    public boolean touchDragged(int x, int y, int pointer) {
+        return false;
+    }
+
+    public boolean mouseMoved(int x, int y) {
+        return false;
+    }
+
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
+}
