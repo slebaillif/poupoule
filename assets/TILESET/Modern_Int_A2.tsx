7c90ecd8e20f1f55b17c8a964bafaf1452884f0c
slebaillif@gmail.com <slebaillif@gmail.com>
2024-06-12 10:29:10 +0300

    healer map

---
+++ assets/TILESET/Modern_Int_A2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Modern_Int_A2" tilewidth="16" tileheight="16" tilecount="768" columns="32">
+ <image source="modern-interiors-tileset-for-rpgs/32px+VXACE/Modern_Int_A2.png" width="512" height="384"/>
+</tileset>
